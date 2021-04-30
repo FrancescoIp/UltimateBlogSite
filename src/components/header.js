05a15f978310eca377e6f9664fea2d988a1da317
Francesco Ippolito <ipp.francesco@gmail.com>
2021-04-30 15:27:04 +0100

    add responsive style for logo and navMenu

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,37 @@
 import React from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby';
 import * as headerStyles from './header.module.scss'
+import Logo from '../images/home/LOGO.png'
 
 const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // Title been switched for a LOGO
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
-return (
-  <header className={headerStyles.header}>
-    <StaticImage src="../images/home/LOGO.png" alt="LOGO" width={300} height={300} />
-    <h1><Link className={headerStyles.title} to="/">{data.site.siteMetadata.title}</Link></h1>
-    <nav>
-      <ul className={headerStyles.navList}>
-        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
-        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About Us</Link></li>
-        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contactUs">Contact Us</Link></li>
-        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
+  return (
+    <header className={headerStyles.header}>
+      <div >
+        <div className={headerStyles.navLogo}>
+          <Link to="/"><img src={Logo} alt="LOGO"></img></Link>
+        </div>
+        <nav className={headerStyles.navMenu}>
+          <ul className={headerStyles.navList}>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About Us</Link></li>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contactUs">Contact Us</Link></li>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
